08a5b8d41880de3f10b8dba684f08f8560b7668a
manjilkoirala <koiralamanjil7@gmail.com>
2025-01-02 18:07:41 +0545

    refactor task controller and validator for improved error handling and pagination support

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,21 +1,22 @@
-import { Request, Response, NextFunction } from "express";
-import { Task } from "../models/task.model";
-import ResponseService from "../services/response.service";
-import { User } from "../models/user.model";
-import { IUser } from "../interfaces/user.interface";
+import { Request, Response, NextFunction } from 'express';
+import { Task } from '../models/task.model';
+import ResponseService from '../services/response.service';
+import { User } from '../models/user.model';
+import { IUser } from '../interfaces/user.interface';
 
 export const createTask = async (
   req: Request & { user?: IUser },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const { title, description, status, priority, deadline } = req.body;
     const userId = req.user?.id as IUser;
+    console.log('Creating task for user:', userId);
 
     const assignee = await User.findById(userId);
     if (!assignee) {
-      return ResponseService.error(res, "User not found", null, 404);
+      return ResponseService.error(res, 'User not found', null, 404);
     }
 
     const newTask = await Task.create({
@@ -27,7 +28,7 @@ export const createTask = async (
       assignee: userId,
     });
 
-    return ResponseService.success(res, "Task created successfully", newTask);
+    return ResponseService.success(res, 'Task created successfully', newTask);
   } catch (error) {
     next(error);
   }
@@ -37,17 +38,43 @@ export const createTask = async (
 export const getTasks = async (
   req: Request & { user?: IUser },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const { role, id } = req.user as IUser;
-    let tasks;
-    if (role === "admin") {
-      tasks = await Task.find();
-    } else {
-      tasks = await Task.find({ assignee: id });
+    const {
+      sortBy = 'createdAt',
+      order = 'asc',
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    //validate pagination query
+    if (isNaN(Number(page)) || isNaN(Number(limit))) {
+      return ResponseService.error(res, 'Invalid pagination query', null, 400);
     }
-    return ResponseService.success(res, "Tasks retrieved successfully", {
+    const pageNumber = Math.max(parseInt(page as string, 10) || 1, 1);
+    const pageSize = Math.max(parseInt(limit as string, 10) || 10, 1);
+
+    //validate
+    const validSortField = ['createdAt', 'deadline', 'priority'];
+    if (!validSortField.includes(sortBy as string)) {
+      return ResponseService.error(res, 'Invalid sort field', null, 400);
+    }
+
+    const sortOrder = order === 'asc' ? 1 : -1;
+    // Define query and pagination logic
+    let query = role === 'admin' ? {} : { assignee: id };
+
+    const tasks = await Task.find(query)
+      .sort({ [sortBy as string]: sortOrder })
+      .skip((pageNumber - 1) * pageSize) // Skip tasks for previous pages
+      .limit(pageSize); // Limit tasks per page
+
+    return ResponseService.success(res, 'Tasks retrieved successfully', {
+      total: tasks.length,
+      page: pageNumber,
+      limit: pageSize,
       tasks: tasks,
     });
   } catch (error) {
@@ -59,22 +86,22 @@ export const getTasks = async (
 export const getTaskbyId = async (
   req: Request & { user?: IUser },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const { id } = req.params;
     const { role, _id } = req.user as IUser;
     const task = await Task.findById({ _id: id });
-    if (!task) return ResponseService.error(res, "Task not found", null, 404);
-    if (role !== "admin" && task.assignee.toString() !== _id.toString()) {
+    if (!task) return ResponseService.error(res, 'Task not found', null, 404);
+    if (role !== 'admin' && task.assignee.toString() !== _id.toString()) {
       return ResponseService.error(
         res,
-        "You are not authorized to view this task",
+        'You are not authorized to view this task',
         null,
-        403
+        403,
       );
     }
-    return ResponseService.success(res, "Task retrieved successfully", task);
+    return ResponseService.success(res, 'Task retrieved successfully', task);
   } catch (error) {
     next(error);
   }
@@ -84,31 +111,31 @@ export const getTaskbyId = async (
 export const updateTask = async (
   req: Request & { user?: IUser },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const { id } = req.params;
     const { role, _id } = req.user as IUser;
     const { title, description, status, priority, deadline } = req.body;
     const task = await Task.findById({ _id: id });
-    if (!task) return ResponseService.error(res, "Task not found", null, 404);
-    if (role !== "admin" && task.assignee.toString() !== _id.toString()) {
+    if (!task) return ResponseService.error(res, 'Task not found', null, 404);
+    if (role !== 'admin' && task.assignee.toString() !== _id.toString()) {
       return ResponseService.error(
         res,
-        "You are not authorized to update this task",
+        'You are not authorized to update this task',
         null,
-        403
+        403,
       );
     }
     const updatedTask = await Task.findByIdAndUpdate(
       { _id: id },
       { title, description, status, priority, deadline },
-      { new: true }
+      { new: true },
     );
     return ResponseService.success(
       res,
-      "Task updated successfully",
-      updatedTask
+      'Task updated successfully',
+      updatedTask,
     );
   } catch (error) {
     next(error);
@@ -119,19 +146,19 @@ export const updateTask = async (
 export const deleteTask = async (
   req: Request & { user?: IUser },
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const { id } = req.params;
     const { role, _id } = req.user as IUser;
     const task = await Task.findById({ _id: id });
-    if (!task) return ResponseService.error(res, "Task not found", null, 404);
-    if (role !== "admin" && task.assignee.toString() !== _id.toString()) {
+    if (!task) return ResponseService.error(res, 'Task not found', null, 404);
+    if (role !== 'admin' && task.assignee.toString() !== _id.toString()) {
       return ResponseService.error(
         res,
-        "You are not authorized to delete this task",
+        'You are not authorized to delete this task',
         null,
-        403
+        403,
       );
     }
     await Task.findByIdAndDelete(id);
